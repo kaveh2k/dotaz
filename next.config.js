2d59b5be2537a92e2de3ebee0ed0d856568c90d3
Kaveh Korki <kavehkorki@gmail.com>
2023-06-20 19:10:13 +1000

    change config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   webpack: (config, options) => {
     config.module.rules.push({
       test: /\.(graphql|gql)/,
